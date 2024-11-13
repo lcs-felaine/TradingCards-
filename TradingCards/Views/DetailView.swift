a67ca61f866021bad3460e412271a60c88dea866
Elaine Fang <efang@lcs.on.ca>
2024-11-12 21:24:09 -0500

    basic colour for the background.

---
+++ TradingCards/Views/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by Fang Elaine on 2024/11/12.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        ZStack{
+            
+            Color.gray
+                .border(.yellow, width: 30)
+                .ignoresSafeArea()
+                .padding(1)
+            
+        }
+    }
+}
+
+#Preview {
+    DetailView()
+}
